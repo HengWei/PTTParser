a5ac677d4bc2e7392ff8a3eeee42776a3eeae12d
HengWei <swatduck@gmail.com>
2018-03-30 14:32:51 +0800

    1. 爬蟲 & 圖片下載

--- crawler.py
+++ crawler.py
@@ -6,16 +6,29 @@ import os
 import urllib
 
 
+
+#================Setting===========================
+
+minPushNumber = 30  #最小推文數
+exclude = ['帥哥']  #排除清單
+targetURL = 'https://www.ptt.cc/bbs/Beauty/index2429.html'
+hostURL = 'https://www.ptt.cc'
+
+#================Setting===========================
+
 def output(filename, sheet, list1):
     book = xlwt.Workbook()
     sh = book.add_sheet(sheet)
-
-
     sh.write(0, 0, '標題')
+    sh.write(0, 1, '推文數')
+    sh.write(0, 2, '連結')
     i = 0
     for item in list1:
         i=i+1
-        sh.write(i, 0, item)
+        sh.write(i, 0, item['title'])
+        sh.write(i, 1, item['pushNumber'])
+        sh.write(i, 2, item['url'])
+
     book.save(filename)
 
 
@@ -31,7 +44,7 @@ def save_img(img_url,file_name,file_path='\img'):
         #拼接图片名（包含路径）
         filename = '{}{}{}{}'.format(file_path,os.sep,file_name,file_suffix)
        #下载图片，并保存到文件夹中
-        urllib.urlretrieve(img_url,filename=filename)
+        urllib.request.urlretrieve(img_url,filename=filename)
     except IOError as e:
         print('文件操作失败', e)
     except Exception as e:
@@ -39,7 +52,7 @@ def save_img(img_url,file_name,file_path='\img'):
 
 
 #目標網址
-targetURL = 'https://www.ptt.cc/bbs/Beauty/index2429.html'
+
 
 r = requests.get(targetURL)
 
@@ -51,21 +64,35 @@ if r.status_code == 200: #200為成功的狀態碼
     a_tag = data.find_all('div', class_='r-ent')
 
     # 個別顯示
-    TitleList = []
-    pushBum = []
-    url = []
+    data=[]
     for item in a_tag:
         if (item.find('a') is not None) and (item.find('span', class_='hl') is not None):
-            # 只顯示30推以上
-            if (item.find('span', class_='hl').string == '爆') or (int(item.find('span', class_='hl').string) > 30):
-                print(item.find('a').string)
-                print(item.find('span', class_='hl').string)
-        # if item.find('a') != None:
-        #     print(item.find('span', class_='hl f3').string)
-        #     TitleList.append(item.find('a').string)
-        #     print(item.find('a').string)
-        # BeautifulSoup(item, 'html.parser').find('a')
-    # output('beati.xls', 'Test1', TitleList)
+            #計算推文數, 爆=100
+            pusnNum = 0
+            if item.find('span', class_='hl').string == '爆':
+                pusnNum = 100
+            else:
+                pusnNum = int(item.find('span', class_='hl').string)
+
+            if pusnNum > minPushNumber:
+                data.append({'title': item.find('a').string, 'pushNumber': pusnNum,
+                             'url': hostURL + item.find('a')['href']})
+                print('標題: '+item.find('a').string)
+                print('推文數: '+ str(pusnNum))
+                print('文章連結: ' + hostURL + item.find('a')['href'])
+                content = requests.get(hostURL + item.find('a')['href'])
+
+                hyperLink = BeautifulSoup(content.text, 'html.parser')
+                if hyperLink is not None:
+                    hyperLink = hyperLink.find_all('a')
+                    no = 1
+                    for pic in hyperLink:
+                        if 'jpg' in pic['href']:
+                            print('圖片下載中...')
+                            save_img(pic['href'], str(no), '\img\\'+item.find('a').string)
+                            no = no+1
+    print(data)
+    output('beati.xls', 'Test1', data)
 else:
     #顯示錯誤訊息
     print('讀取網頁失敗，請確認網址: ' + targetURL)
